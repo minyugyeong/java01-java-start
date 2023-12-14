54288498a0d0b0af3a423002d44b905fc286c8b1
minyugyeong <alsdbrud123@naver.com>
2023-12-14 18:23:14 +0900

    섹션2 - 변수

---
+++ src/variable/Var7.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true; //불리언(boolean) true, false 입력 가능
+        char d = 'A'; //문자 하나
+        String e = "Hello Java"; //문자열, 문자열을 다루기 위한 특별한 타입
+
+        //리터럴 : 코드에서 개발자가 직접 적은 100, 10.5, true, 'A', 'Hello Java'와
+        //같은 고정된 값을 프로그래밍 용어로 리터럴(literal)이라 한다.
+        //변수의 값은 변할 수 있지만 리터럴은 개발자가 직접 입력한 고정된 값이다. 따라서 리터럴 자체는 변하지 않는다.
+    }
+}
