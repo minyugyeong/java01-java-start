0c803151d075a15e469eb70e4b5de78a552a1aaa
minyugyeong <alsdbrud123@naver.com>
2023-12-20 18:28:41 +0900

    섹션5 - 반복문

---
+++ src/loop/ex/ForEx1.java
@@ -0,0 +1,12 @@
+package loop.ex;
+
+public class ForEx1 {
+    public static void main(String[] args) {
+        //처음 10개의 자연수를 출력하는 프로그램을 작성해 보세요. 이때, count 라는 변수를 사용해야 합니다.
+        //while문, for문 2가지 버전의 정답을 만들어야 합니다.
+
+        for(int count = 1; count <= 10; count++){
+            System.out.println(count);
+        }
+    }
+}
---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,28 @@
+package loop.ex;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        //반복문을 사용하여 처음 10개의 짝수를 출력하는 프로그램을 작성해 보세요.
+        //이때, num 이라는 변수를 사용하여 수를 표현해야 합니다.
+        //while, for문 2가지 버전의 정답을 만들어야 합니다.
+        int num = 2;
+        int count = 1;
+
+//        while(num <= 10){
+//            if(num % 2 != 0) {
+//                num++;
+//                continue;
+//            }else{
+//                System.out.println(num);
+//                num++;
+//            }
+//        }
+
+        while(count <= 10){
+            System.out.println(num);
+            num += 2;
+            count ++;
+        }
+    }
+
+}
